91209a6d82320951562cbbc0ac92de5c24d659bf
Adam G <adamgibs@outlook.com>
2016-01-10 14:42:59 -0500

    bubble sort method built

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,2 +1,16 @@
+
 def bubble_sort(array)
+	swapped = true
+	while swapped
+		swapped = false
+		n = array.length - 2
+		for i in 0..n do
+			if array[i + 1] < array[i]
+				array[i + 1], array[i] = array[i], array[i + 1]
+				swapped = true
+			end
+		end
+	end
+	array
 end
+p bubble_sort([4,32,6,5,4]) 
